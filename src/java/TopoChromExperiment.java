0b146ff7a6ff4d880c2852ab614bcfdc90236337
Pedro Urbina <pedro.urbina@gmail.com>
2016-07-22 18:05:35 -0400

    commented on first commit, ctrl+f on ? to see some uncertanties

--- src/java/TopoChromExperiment.java
+++ src/java/TopoChromExperiment.java
@@ -3,5 +3,9 @@ package java;
 /**
  * Created by snakecharmer1024 on 7/19/16.
  */
+
+// possible main insertion?
+
 public class TopoChromExperiment {
+	
 }
--- src/java/TransactionPermutation.java
+++ src/java/TransactionPermutation.java
@@ -9,14 +9,23 @@ import java.util.List;
 /**
  * Created by snakecharmer1024 on 7/20/16.
  */
-public class TransactionPermutation
-{
-    public Sha256Hash txHash;
-    public Peer entryPeer;
-    public List<Integer> permutation;
+
+// This class is created to track the ordinality of PeerGroup 
+// transmissions with regard to a specific transaction.
+
+public class TransactionPermutation {
+
+    public Sha256Hash txHash; //SHA256 encoded transaction information
+    public Peer entryPeer; // is entryPeer self? (ie entry node of network)
+    public List<Integer> permutation; 
+    /* array of n integers where n is the size of the peer group 
+	the integers represent the index number of the original peer group ordering
+    will be updated by TransactionReceivedListener
+    */
 
     public TransactionPermutation(Peer entryPeer, Sha256Hash txHash)
     {
+    	//constructor
         this.txHash = txHash;
         this.entryPeer = entryPeer;
         this.permutation = new ArrayList<Integer>();
